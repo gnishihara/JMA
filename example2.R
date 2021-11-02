f781466ec560ee3868ce3cc18f1f27a93c905be2
Greg Nishihara <gnishihara@gmail.com>
2021-11-02 09:11:30 +0900

    Modified code, consider removing script.

--- example2.R
+++ example2.R
@@ -7,48 +7,62 @@ source("scrape_jma_table.R")
 
 prec_no = read_csv("list_of_prec_no.csv")
 block_no = scrape_block_no(prec_no = 84)
-#
+
 # arikawa = block_no %>% filter(str_detect(block, "有川")) %>% slice(2)
-#
-# arikawa2017 =
-#   arikawa %>%
-#   mutate(year = 2017) %>%
-#   unnest(year) %>%
-#   mutate(month = list(1:12)) %>%
-#   unnest(month) %>%
-#   mutate(day = map(month, function(x) {
-#     1:days_in_month(x)
-#   })) %>% unnest(day) %>%
-#   mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
-#   select(block, data)
-#
-# arikawa2018 = arikawa %>%
-#   mutate(year = 2018) %>%
-#   unnest(year) %>%
-#   mutate(month = list(1:12)) %>%
-#   unnest(month) %>%
-#   mutate(day = map(month, function(x) {
-#     1:days_in_month(x)
-#   })) %>% unnest(day) %>%
-#   mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
-#   select(block, data)
-#
-# arikawa2019 = arikawa %>%
-#   mutate(year = 2019) %>%
-#   unnest(year) %>%
-#   mutate(month = list(1:10)) %>%
-#   unnest(month) %>%
-#   mutate(day = map(month, function(x) {
-#     1:days_in_month(x)
-#   })) %>% unnest(day) %>%
-#   mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
-#   select(block, data)
+
+arikawa = block_no %>% filter(str_detect(block, "有川")) %>% slice(2) %>%
+  mutate(year = list(2017:2020)) %>%
+  unnest(year) %>%
+  mutate(month = list(1:12)) %>%
+  unnest(month) %>%
+  mutate(day = map(month, function(x) {
+    1:days_in_month(x)
+  })) %>% unnest(day) %>%
+  mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
+  select(block, data) %>%
+  unnest(data) %>%
+  mutate(H = hour(datetime) + minute(datetime)/60)
+
+arikawa %>% write_csv("~/Lab_Data/weather/201701_202012_Arikawa_JMA_Data.csv")
+
+arikawa2021 = block_no %>% filter(str_detect(block, "有川")) %>% slice(2) %>%
+  mutate(year = 2021) %>%
+  unnest(year) %>%
+  mutate(month = list(1:5)) %>%
+  unnest(month) %>%
+  mutate(day = map(month, function(x) {
+    1:days_in_month(x)
+  })) %>% unnest(day) %>%
+  mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
+  select(block, data)%>%
+  unnest(data) %>%
+  mutate(H = hour(datetime) + minute(datetime)/60)
+
+arikawa2021 %>% write_csv(file = "~/Lab_Data/weather/202101_202105_Arikawa_JMA_Data.csv")
+
+
+################################################################################
+arikawa202009 = block_no %>% filter(str_detect(block, "有川")) %>% slice(2) %>%
+  mutate(year = 2020) %>%
+  unnest(year) %>%
+  mutate(month = list(9)) %>%
+  unnest(month) %>%
+  mutate(day = map(month, function(x) {
+    1:14
+  })) %>% unnest(day) %>%
+  mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
+  select(block, data)%>%
+  unnest(data) %>%
+  mutate(H = hour(datetime) + minute(datetime)/60)
+arikawa202009 %>%  write_csv(path = "~/Lab_Data/weather/20209_01-14_Arikawa_JMA_Data.csv")
+
+################################################################################
 
 
 fukue = block_no %>% filter(str_detect(block, "福江"))
 
-fukue2017 = fukue %>%
-  mutate(year = 2017) %>%
+fukue = fukue %>%
+  mutate(year = list(2017:2020)) %>%
   unnest(year) %>%
   mutate(month = list(1:12)) %>%
   unnest(month) %>%
@@ -56,37 +70,31 @@ fukue2017 = fukue %>%
     1:days_in_month(x)
   })) %>% unnest(day) %>%
   mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
-  select(block, data)
+  select(block, data)%>%
+  unnest(data) %>%
+  mutate(H = hour(datetime) + minute(datetime)/60)
 
-fukue2018 = fukue %>%
-  mutate(year = 2018) %>%
+fukueB = block_no %>% filter(str_detect(block, "福江")) %>%
+  mutate(year = 2021) %>%
   unnest(year) %>%
-  mutate(month = list(1:12)) %>%
+  mutate(month = list(1:2)) %>%
   unnest(month) %>%
   mutate(day = map(month, function(x) {
     1:days_in_month(x)
   })) %>% unnest(day) %>%
   mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
-  select(block, data)
+  select(block, data)%>%
+  unnest(data) %>%
+  mutate(H = hour(datetime) + minute(datetime)/60)
 
-fukue2019 = fukue %>%
-  mutate(year = 2019) %>%
-  unnest(year) %>%
-  mutate(month = list(1:10)) %>%
-  unnest(month) %>%
-  mutate(day = map(month, function(x) {
-    1:days_in_month(x)
-  })) %>% unnest(day) %>%
-  mutate(data = pmap(list(year, month, day, prec_no, block_no, kubun), get_hpa)) %>%
-  select(block, data)
+fukue %>% write_csv(file = "~/Lab_Data/weather/201701_202012_Fukue_JMA_Data.csv")
+fukueB %>% write_csv(file = "~/Lab_Data/weather/202101_202102_Fukue_JMA_Data.csv")
 
 
 
-# arikawa = arikawa2017 %>% bind_rows(arikawa2018) %>% bind_rows(arikawa2019)
-fukue = fukue2017 %>% bind_rows(fukue2018) %>% bind_rows(fukue2019)
 
-# arikawa = arikawa %>% unnest(data) %>% mutate(H = hour(datetime) + minute(datetime)/60)
-fukue = fukue %>% unnest(data) %>% mutate(H = hour(datetime) + minute(datetime)/60)
 
-# write.csv(arikawa, file = "arikawa_2017to2019_10.csv")
-write.csv(fukue, file = "fukue_2017to2019_10.csv")
+
+
+
+
--- example3.R
+++ example3.R
@@ -5,34 +5,32 @@ library(lubridate)
 fukue = read_csv("fukue_2017to2019_10.csv")
 arikawa = read_csv("arikawa_2017to2019_10.csv")
 
-arikawa %>% filter(year(datetime) == 2017) %>% select(-X1) %>%
-  write_csv("Arikawa_2017.csv")
+# arikawa %>% filter(year(datetime) == 2017) %>% select(-X1) %>% write_csv("Arikawa_2017.csv")
 
 
 library(sugrrants)
 
-p1 =
-  fukue %>% mutate(year = year(datetime)) %>%
-  filter(year == 2017) %>%
-  mutate(date = as_date(datetime)) %>%
-  ggplot() +
-  geom_point(aes(x = H, y = hpa)) +
-  facet_calendar(date)
-ggsave(file = "fukue2017.png", p1, width = 841, height = 594, units="mm")
-p2 = fukue %>% mutate(year = year(datetime)) %>%
-  filter(year == 2018) %>%
-  mutate(date = as_date(datetime)) %>%
-  ggplot() +
-  geom_point(aes(x = H, y = hpa)) +
-  facet_calendar(date)
-ggsave(file = "fukue2018.png", p1, width = 841, height = 594, units="mm")
-p3 = fukue %>% mutate(year = year(datetime)) %>%
-  filter(year == 2019) %>%
-  mutate(date = as_date(datetime)) %>%
-  ggplot() +
-  geom_point(aes(x = H, y = hpa)) +
-  facet_calendar(date)
-ggsave(file = "fukue2019.png", p1, width = 841, height = 594, units="mm")
+# p1 = fukue %>% mutate(year = year(datetime)) %>%
+#   filter(year == 2017) %>%
+#   mutate(date = as_date(datetime)) %>%
+#   ggplot() +
+#   geom_point(aes(x = H, y = hpa)) +
+#   facet_calendar(date)
+# ggsave(file = "fukue2017.png", p1, width = 841, height = 594, units="mm")
+# p2 = fukue %>% mutate(year = year(datetime)) %>%
+#   filter(year == 2018) %>%
+#   mutate(date = as_date(datetime)) %>%
+#   ggplot() +
+#   geom_point(aes(x = H, y = hpa)) +
+#   facet_calendar(date)
+# ggsave(file = "fukue2018.png", p1, width = 841, height = 594, units="mm")
+# p3 = fukue %>% mutate(year = year(datetime)) %>%
+#   filter(year == 2019) %>%
+#   mutate(date = as_date(datetime)) %>%
+#   ggplot() +
+#   geom_point(aes(x = H, y = hpa)) +
+#   facet_calendar(date)
+# ggsave(file = "fukue2019.png", p1, width = 841, height = 594, units="mm")
 
 
 
